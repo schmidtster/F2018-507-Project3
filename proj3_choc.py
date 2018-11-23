0b8991864a8c8cbf4851dd459b4ae52c1300c9ec
schmidtster <41802830+schmidtster@users.noreply.github.com>
2018-11-22 21:10:39 -0500

    Version 5
    Hopefully this will be the final version

--- proj3_choc.py
+++ proj3_choc.py
@@ -1,7 +1,6 @@
 import sqlite3 as sqlite3
 import csv
 import json
-import pprint
 
 # proj3_choc.py
 # You can change anything in this file you want as long as you pass the tests
@@ -134,16 +133,19 @@ def process_command(command):
             "sourcecountry": ("c1.Alpha2", "Bars.BroadBeanOriginId"),
             "sellregion": ("c1.Region", "Bars.CompanyLocationId"),
             "sourceregion": ("c1.Region", "Bars.BroadBeanOriginId")}
-        used_commands = ['AS c1 ON Bars.CompanyLocationId = c1.Id JOIN Countries AS c2 ON Bars.BroadBeanOriginId = c2.Id', 'ORDER BY Bars.Rating DESC', 'LIMIT 10']
+        used_commands = ['AS c1 ON Bars.CompanyLocationId = c1.Id JOIN Countries AS c2 ON Bars.BroadBeanOriginId = '
+                         'c2.Id', 'ORDER BY Bars.Rating DESC', 'LIMIT 10']
         for parameter_per_command in parameters[1:]:
             if "=" in parameter_per_command:
                 parameter = parameter_per_command.split("=")
                 if parameter[0] in ["sellcountry", "sourcecountry"]:
                     columns = column_name[parameter[0]]
-                    used_commands[0] = "AS c1 ON {} = '{}' AND {} = c1.Id JOIN Countries as c2 ON c2.Id = {}".format(columns[0], parameter[1], columns[1], columns[1])
+                    used_commands[0] = "AS c1 ON {} = '{}' AND {} = c1.Id JOIN Countries as c2 ON c2.Id = {}" \
+                                       "".format(columns[0], parameter[1], columns[1], columns[1])
                 elif parameter[0] in ["sellregion", "sourceregion"]:
                     columns = column_name[parameter[0]]
-                    used_commands[0] = "AS c1 ON {} = '{}' AND {} = c1.Id JOIN Countries as c2 ON c2.Id = {}".format(columns[0], parameter[1], columns[1], columns[1])
+                    used_commands[0] = "AS c1 ON {} = '{}' AND {} = c1.Id JOIN Countries as c2 ON c2.Id = {}" \
+                                       "".format(columns[0], parameter[1], columns[1], columns[1])
                 elif parameter[0] == "top":
                     top = parameter[1]
                     used_commands[2] = "LIMIT {}".format(int(top))
@@ -165,14 +167,16 @@ def process_command(command):
                 print("error", parameter_per_command)
                 # bogus parameter throw exception
         statement_bars = 'SELECT Bars.SpecificBeanBarName, Bars.Company, c1.EnglishName, Bars.Rating, ' \
-                         'Bars.CocoaPercent, c2.EnglishName FROM Bars JOIN Countries {} {} {}'.format(used_commands[0], used_commands[1], used_commands[2])
+                         'Bars.CocoaPercent, c2.EnglishName FROM Bars JOIN Countries {} {} {}' \
+                         ''.format(used_commands[0], used_commands[1], used_commands[2])
         # print(statement_bars)
         execute = cur.execute(statement_bars)
         results_bars = execute.fetchall()
         return results_bars
 
     if parameters[0] == "companies":
-        used_commands = ["AVG(Bars.Rating) AS average", "AS c1 ON c1.Id = Bars.CompanyLocationId", "ORDER BY average DESC", "LIMIT 10"]
+        used_commands = ["AVG(Bars.Rating) AS average", "AS c1 ON c1.Id = Bars.CompanyLocationId",
+                         "ORDER BY average DESC", "LIMIT 10"]
         column_name = {
             "country": ("c1.Alpha2", "Bars.CompanyLocationId"),
             "region": ("c1.Region", "Bars.CompanyLocationId")
@@ -182,10 +186,12 @@ def process_command(command):
                 parameter = parameter_per_command.split("=")
                 if parameter[0] == "country":
                     columns = column_name[parameter[0]]
-                    used_commands[1] = "AS c1 ON {} = '{}' AND {} = c1.Id JOIN Countries as c2 ON c2.Id = {}".format(columns[0], parameter[1], columns[1], columns[1])
+                    used_commands[1] = "AS c1 ON {} = '{}' AND {} = c1.Id JOIN Countries as c2 ON c2.Id = {}" \
+                                       "".format(columns[0], parameter[1], columns[1], columns[1])
                 elif parameter[0] == "region":
                     columns = column_name[parameter[0]]
-                    used_commands[1] = "AS c1 ON {} = '{}' AND {} = c1.Id JOIN Countries as c2 ON c2.Id = {}".format(columns[0], parameter[1], columns[1], columns[1])
+                    used_commands[1] = "AS c1 ON {} = '{}' AND {} = c1.Id JOIN Countries as c2 ON c2.Id = {}" \
+                                       "".format(columns[0], parameter[1], columns[1], columns[1])
                 elif parameter[0] == "top":
                     top = parameter[1]
                     used_commands[3] = "LIMIT {}".format(int(top))
@@ -202,14 +208,17 @@ def process_command(command):
             if "bars_sold" == parameter_per_command:
                 used_commands[0] = "COUNT(Bars.SpecificBeanBarName) AS count_bars"
                 used_commands[2] = "ORDER BY COUNT(Bars.SpecificBeanBarName) DESC"
-        statement_companies = 'SELECT Bars.Company, c1.EnglishName, {} FROM Bars JOIN Countries {} GROUP BY Bars.Company HAVING COUNT(Bars.Company) > 4 {} {}'.format(used_commands[0], used_commands[1], used_commands[2], used_commands[3])
+        statement_companies = 'SELECT Bars.Company, c1.EnglishName, {} FROM Bars JOIN Countries {} GROUP BY ' \
+                              'Bars.Company HAVING COUNT(Bars.Company) > 4 {} {}' \
+                              ''.format(used_commands[0], used_commands[1], used_commands[2], used_commands[3])
         # print(statement_companies)
         execute = cur.execute(statement_companies)
         results_companies = execute.fetchall()
         return results_companies
 
     if parameters[0] == "countries":
-        used_commands = ["AVG(Bars.Rating) AS average", "AS c1 ON", "c1.Id = Bars.CompanyLocationId", "Bars.CompanyLocationId", "average", "ORDER BY average DESC", "LIMIT 10"]
+        used_commands = ["AVG(Bars.Rating) AS average", "AS c1 ON", "c1.Id = Bars.CompanyLocationId",
+                         "Bars.CompanyLocationId", "average", "ORDER BY average DESC", "LIMIT 10"]
         column_name = {
             "sellers": ("c1.Region", "Bars.CompanyLocationId"),
             "sources": ("c1.Region", "Bars.BroadBeanOriginId")
@@ -250,14 +259,22 @@ def process_command(command):
             else:
                 # print("error")
                 continue
-        statement_countries = 'SELECT c1.EnglishName, c1.Region, {} FROM Bars JOIN Countries {} {} GROUP BY {} HAVING {} > 4 {} {}'.format(used_commands[0], used_commands[1], used_commands[2], used_commands[3], used_commands[4], used_commands[5], used_commands[6])
+        statement_countries = 'SELECT c1.EnglishName, c1.Region, {} FROM Bars JOIN Countries {} {} GROUP BY {} ' \
+                              'HAVING {} > 4 {} {}'.format(used_commands[0],
+                                                           used_commands[1],
+                                                           used_commands[2],
+                                                           used_commands[3],
+                                                           used_commands[4],
+                                                           used_commands[5],
+                                                           used_commands[6])
         # print(statement_countries)
         execute = cur.execute(statement_countries)
         results_countries = execute.fetchall()
         return results_countries
 
     if parameters[0] == "regions":
-        used_commands = ["AVG(Bars.Rating) AS average", "Bars.CompanyLocationId", "average", "ORDER BY average DESC", "LIMIT 10"]
+        used_commands = ["AVG(Bars.Rating) AS average", "Bars.CompanyLocationId", "average", "ORDER BY average DESC",
+                         "LIMIT 10"]
         column_name = {
             "sellers": ("c1.Region", "Bars.CompanyLocationId"),
             "sources": ("c1.Region", "Bars.BroadBeanOriginId")
@@ -295,8 +312,12 @@ def process_command(command):
             else:
                 # print("error")
                 continue
-        statement_regions = 'SELECT c1.Region, {} FROM Bars JOIN Countries AS c1 ON c1.Id = {} GROUP BY c1.Region HAVING {} > 4 {} {}'.format(
-            used_commands[0], used_commands[1], used_commands[2], used_commands[3], used_commands[4])
+        statement_regions = 'SELECT c1.Region, {} FROM Bars JOIN Countries AS c1 ON c1.Id = {} GROUP BY c1.Region ' \
+                            'HAVING {} > 4 {} {}'.format(used_commands[0],
+                                                         used_commands[1],
+                                                         used_commands[2],
+                                                         used_commands[3],
+                                                         used_commands[4])
         # print(statement_regions)
         execute = cur.execute(statement_regions)
         results_regions = execute.fetchall()
@@ -311,7 +332,8 @@ def load_help_text():
 # Part 3: Implement interactive prompt. We've started for you!
 def interactive_prompt():
     help_text = load_help_text()
-    bars_commands = ["bars", "sellcountry", "sourcecountry", "sellregion", "sourceregion", "ratings", "cocoa", "top", "bottom"]
+    bars_commands = ["bars", "sellcountry", "sourcecountry", "sellregion", "sourceregion", "ratings", "cocoa", "top",
+                     "bottom"]
     countries_commands = ["countries", "region", "sellers", "sources", "ratings", "cocoa", "bars_sold", "top", "bottom"]
     companies_commands = ["companies", "country", "region", "ratings", "cocoa", "bars_sold", "top", "bottom"]
     regions_commands = ["regions", "sellers", "sources", "ratings", "cocoa", "bars_sold", "top", "bottom"]
@@ -342,7 +364,12 @@ def interactive_prompt():
             command_processed = process_command(command_string)
             for each_result in command_processed:
                 # pp.pprint(each_result)
-                print("{:<30.30} | {:<30.30} | {:<25.25} | {:<5} | {:<5} | {:<25.25}".format(each_result[0], each_result[1], each_result[2], each_result[3], each_result[4], each_result[5]))
+                print("{:<30.30} | {:<30.30} | {:<25.25} | {:<5} | {:<5} | {:<25.25}".format(each_result[0],
+                                                                                             each_result[1],
+                                                                                             each_result[2],
+                                                                                             each_result[3],
+                                                                                             each_result[4],
+                                                                                             each_result[5]))
 
         elif countries_commands[0] in response_list:
             command_list = ["", "", "", "", ""]
@@ -369,8 +396,7 @@ def interactive_prompt():
                     command_string += " " + params
             command_processed = process_command(command_string)
             for each_result in command_processed:
-                print(each_result)
-                # print("{:<30.30} | {:<25.25} | {:<5}".format(each_result[0], each_result[1]), each_result[2])
+                print("{:<30.30} | {:<25.25} | {:<5}".format(each_result[0], each_result[1], each_result[2]))
 
         elif companies_commands[0] in response_list:
             command_list = ["", "", "", ""]
@@ -394,7 +420,7 @@ def interactive_prompt():
                     command_string += " " + params
             command_processed = process_command(command_string)
             for each_result in command_processed:
-                print("{:<30.30} | {:<25.25} | {:<5}".format(each_result[0], each_result[1]), each_result[2])
+                print("{:<30.30} | {:<25.25} | {:<5}".format(each_result[0], each_result[1], each_result[2]))
 
         elif regions_commands[0] in response_list:
             command_list = ["regions", "", "", ""]
@@ -426,6 +452,7 @@ def interactive_prompt():
 
         elif response == "exit":
             response = response
+            print("Exiting the program...")
 
         else:
             print("Command not recognized: ", response)
@@ -434,10 +461,4 @@ def interactive_prompt():
 
 # Make sure nothing runs or prints out when this file is run as a module
 if __name__ == "__main__":
-    # clean_database()
-    # read_csv()
-    # read_json()
-    # update_country_codes()
-    # print(process_command('ratings sellers regions top=10'))
     interactive_prompt()
-
